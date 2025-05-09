0599a401c8c520027d33f9f8455cca9761a533d5
MobinEffati <MobinEffati@gmail.com>
2025-05-09 20:27:00 +0330

    Add Account Controller

---
+++ MBIN.Web/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MBIN.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
